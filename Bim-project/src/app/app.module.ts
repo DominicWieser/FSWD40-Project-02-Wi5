dab7539fd0151a8530f16eda62e9fa6b61a98692
Denis Schuele <schueledenis@gmx.at>
2018-06-04 12:00:27 +0200

    basic

--- Bim-project/src/app/app.module.ts
+++ Bim-project/src/app/app.module.ts
@@ -2,16 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './src/app/navbar/navbar.component';
-import { FooterComponent } from './src/app/footer/footer.component';
-import { HeroComponent } from './src/app/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    HeroComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule
